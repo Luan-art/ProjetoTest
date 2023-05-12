6a9ead4a4cd8b6b2bac13689e60173b02904c329
Luan-art <74328280+Luan-art@users.noreply.github.com>
2023-05-12 11:22:39 -0300

    Add files via upload

---
+++ ProjetoTeste/src/main/java/controller/LoginServlet.java
@@ -0,0 +1,51 @@
+package controller;
+
+import java.io.IOException;
+
+import dao.EmployeeDao;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Employee;
+
+@WebServlet("/login")
+
+public class LoginServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	EmployeeDao employeeDao = new EmployeeDao();
+
+
+	public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/login.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("login");
+        String password = request.getParameter("password");
+        
+        try {
+            employeeDao.getUser(username);
+            employeeDao.getPassword(password);
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/detalhesUser.jsp");
+		dispatcher.forward(request, response);
+    }
+	
+}
